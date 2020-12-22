49224bd80325818ae6da9e231ebed56b50b28fff
shessafridi <mshesskhan@gmail.com>
2020-12-23 01:48:49 +0500

    Removed Unused Code

--- src/common/SingleResource.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Redirect } from 'react-router';
-
-const SingleResource: React.FC<any> = ({ resource }) => {
-  return <Redirect to={`${resource}/1`} />;
-};
-
-export default SingleResource;
