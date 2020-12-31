b443929b97f4841b1b2a365b4105915926539acf
shessafridi <mshesskhan@gmail.com>
2020-12-31 19:33:58 +0500

    Fixed a bug in image service to check for null values

--- src/middleware/uploadImages.ts
+++ src/middleware/uploadImages.ts
@@ -2,8 +2,12 @@ import { ResourceData } from '../models/ResourceData';
 import imageUploadService from '../services/imageUploadService';
 
 export async function uploadImagesIfExist(data: ResourceData) {
+  const filtered = Object.entries(data.imageUploaders).filter(
+    ([path, files]) => files !== null
+  );
+
   if (data.imageUploaders) {
-    const images = Object.entries(data.imageUploaders).map(([path, files]) => ({
+    const images = filtered.map(([path, files]) => ({
       path: path.replace(/_/g, '.'),
       files,
     }));
