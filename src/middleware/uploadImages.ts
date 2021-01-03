7ae1b875499e6348de39730713ab52e6a25550ba
shessafridi <mshesskhan@gmail.com>
2021-01-03 21:36:17 +0500

    Fixed a small bug in the image uploader

--- src/middleware/uploadImages.ts
+++ src/middleware/uploadImages.ts
@@ -2,11 +2,10 @@ import { ResourceData } from '../models/ResourceData';
 import imageUploadService from '../services/imageUploadService';
 
 export async function uploadImagesIfExist(data: ResourceData) {
-  const filtered = Object.entries(data.imageUploaders).filter(
-    ([path, files]) => files !== null
-  );
-
   if (data.imageUploaders) {
+    const filtered = Object.entries(data.imageUploaders).filter(
+      ([path, files]) => files !== null
+    );
     const images = filtered.map(([path, files]) => ({
       path: path.replace(/_/g, '.'),
       files,
